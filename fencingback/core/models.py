e391581da6d60ee02f830da87959195d5020a552
Gvozzdd <156453933+Gvozzdd@users.noreply.github.com>
2025-04-29 11:49:23 +0500

    Update models.py

---
+++ fencingback/core/models.py
@@ -0,0 +1,28 @@
+# core/models.py
+from django.db import models
+
+class News(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+class Review(models.Model):
+    author = models.CharField(max_length=100)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Review by {self.author}'
+
+class Enrollment(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=15)
+    age = models.IntegerField()
+    style = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'Enrollment by {self.name} for {self.style}'
